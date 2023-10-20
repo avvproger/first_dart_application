89497b28c8fb9eb5a6440cea97db7a9c9a73305b
avvproger <avvwdesign@gmail.com>
2023-10-21 00:35:03 +0700

    first commit

---
+++ bin/first_dart_application.dart
@@ -0,0 +1,5 @@
+import 'package:first_dart_application/first_dart_application.dart' as first_dart_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_dart_application.calculate()}!');
+}
---
+++ test/first_dart_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_dart_application/first_dart_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
